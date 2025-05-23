099c410642b1b10b42994868e553d8db63eb030f
Dominik Palo <dominik.palo@student.tuke.sk>
2025-05-23 18:12:43 +0200

    added config for be

---
+++ backend/app/config.py
@@ -0,0 +1,18 @@
+# backend/app/config.py
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # Načíta premenné z .env súboru
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://user:password@host:port/db")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "default_secret_key")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+
+settings = Settings()

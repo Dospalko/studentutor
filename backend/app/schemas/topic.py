9ce67ad7ec7a1d91a6c42cdfcc50c702e5e63349
Dominik Palo <dominik.palo@student.tuke.sk>
2025-06-03 13:26:38 +0200

    feat: topic schema

---
+++ backend/app/schemas/topic.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from ..db.enums import TopicStatus, UserDifficulty # Importuj enumy z ich nového miesta
+class TopicBase(BaseModel):
+      name: str = Field(min_length=1, max_length=255)
+      user_strengths: Optional[str] = None
+      user_weaknesses: Optional[str] = None
+      user_difficulty: Optional[UserDifficulty] = None
+      status: Optional[TopicStatus] = TopicStatus.NOT_STARTED
+
+class TopicCreate(TopicBase):
+      pass
+
+class TopicUpdate(BaseModel):
+      name: Optional[str] = Field(None, min_length=1, max_length=255)
+      user_strengths: Optional[str] = None
+      user_weaknesses: Optional[str] = None
+      user_difficulty: Optional[UserDifficulty] = None
+      status: Optional[TopicStatus] = None
+
+class Topic(TopicBase):
+      id: int
+      subject_id: int
+      status: TopicStatus
+
+      class Config:
+          from_attributes = True

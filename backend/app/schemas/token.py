17ef5ad04f5975182798bf47a3b4749fb82e27f5
Dominik Palo <dominik.palo@student.tuke.sk>
2025-06-03 13:28:04 +0200

    feat: token schema

---
+++ backend/app/schemas/token.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None

554d2bf9fde8ff97c99f3ae40892d327f0d552c2
Dominik Palo <dominik.palo@student.tuke.sk>
2025-06-13 12:57:17 +0200

    feat: more achievments adding

--- backend/app/db/enums.py
+++ backend/app/db/enums.py
@@ -35,12 +35,17 @@ class MaterialTypeEnum(str, enum.Enum):
     KNIHA = "kniha"
     INE = "ine"
 
-
+# Enum pre typy kritérií achievementov
 class AchievementCriteriaType(str, enum.Enum):
-    SUBJECTS_CREATED = "subjects_created" # Počet vytvorených predmetov
-    TOPICS_COMPLETED = "topics_completed" # Počet dokončených tém celkovo
-    TOPICS_COMPLETED_IN_SUBJECT = "topics_completed_in_subject" # Počet dokončených tém v jednom predmete
-    STUDY_BLOCKS_COMPLETED = "study_blocks_completed" # Počet dokončených študijných blokov
-    LOGIN_STREAK = "login_streak" # Koľko dní po sebe sa prihlásil (pokročilejšie)
-    PERFECT_SCORE_TOPIC = "perfect_score_topic" # Ak by si mal testovanie
+    SUBJECTS_CREATED = "subjects_created"
+    TOPICS_COMPLETED = "topics_completed"
     FIRST_PLAN_GENERATED = "first_plan_generated"
+    TOPICS_IN_SUBJECT_COMPLETED_PERCENT = "topics_in_subject_completed_percent"
+    SUBJECT_FULLY_COMPLETED = "subject_fully_completed"
+    STUDY_MATERIALS_UPLOADED_PER_SUBJECT = "study_materials_uploaded_per_subject"
+    # CONSECUTIVE_STUDY_DAYS = "consecutive_study_days" # Zatiaľ vynecháme, vyžaduje zložitejšie sledovanie
+    TOTAL_STUDY_BLOCKS_COMPLETED = "total_study_blocks_completed"
+    TOPICS_CREATED_PER_SUBJECT = "topics_created_per_subject"
+    PLANS_GENERATED_OR_UPDATED = "plans_generated_or_updated"
+    # BLOCKS_COMPLETED_ON_TIME = "blocks_completed_on_time" # Zatiaľ vynecháme
+    TOTAL_MATERIALS_UPLOADED = "total_materials_uploaded"

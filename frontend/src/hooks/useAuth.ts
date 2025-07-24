70dcc548b08e8f34408ec62c07ef7e816a131ea8
Dominik Palo <dominik.palo@student.tuke.sk>
2025-07-24 21:18:23 +0200

    feat: commnet update

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -2,8 +2,6 @@ import { useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
 
 /**
- * Zaručuje, že vždy dostaneš NON-undefined kontext.
- * Ak by niekde chýbal <AuthProvider>, vyhodí jasnú chybu.
  */
 export const useAuth = () => {
   const ctx = useContext(AuthContext);
